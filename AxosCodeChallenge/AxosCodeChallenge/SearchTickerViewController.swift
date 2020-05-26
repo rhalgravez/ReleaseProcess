c499a9ef88f8e1d229c2fc52bc79da9c9127302b
Roberto Halgravez <r.halgravez@gmail.com>
2020-05-26 01:29:43 -0600

    Add logo image for the SearchTickerViewController

--- AxosCodeChallenge/AxosCodeChallenge/SearchTickerViewController.swift
+++ AxosCodeChallenge/AxosCodeChallenge/SearchTickerViewController.swift
@@ -10,20 +10,37 @@ import UIKit
 
 class SearchTickerViewController: UIViewController {
     
-    let searchButton = AXIButton(title: "Show global equitiy")
+    private let logoImageView = UIImageView()
+    private let searchButton = AXIButton(title: "Show global equitiy")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        configureLogoImageView()
         configureSearchButton()
+    }
+    
+    private func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "axosinvest-logo")
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 195),
+            logoImageView.widthAnchor.constraint(equalToConstant: 380)
+            
+            
+        ])
     }
     
     private func configureSearchButton() {
         view.addSubview(searchButton)
         
         NSLayoutConstraint.activate([
-            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 132),
+            searchButton.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 16),
             searchButton.leadingAnchor.constraint(equalTo:
                 view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
             searchButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
